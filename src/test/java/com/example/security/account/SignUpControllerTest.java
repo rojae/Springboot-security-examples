4fa7be81a9b97ac083932c105a1c2b35d701107c
rojae <Jae.Thon96@gmail.com>
2020-11-03 22:54:18 +0900

    ADD) Security CSRF token Mock Test

---
+++ src/test/java/com/example/security/account/SignUpControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.security.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SignUpControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    // csrf test
+    @Test
+    public void signUpForm() throws Exception{
+        mockMvc.perform(get("/signup"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("_csrf")));
+    }
+
+    // csrf test
+    @Test
+    @Transactional
+    public void processSignUp() throws Exception{
+        mockMvc.perform(post("/signup")
+                .param("username", "rojae")
+                .param("password","123")
+                .with(csrf())
+        ).andDo(print()).andExpect(status().is3xxRedirection());
+
+    }
+
+
+}
